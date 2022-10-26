13c85759e680cf209ba70dc2d9fd5d9a78211d7d
Nahid Ibn Ali <nahidibnali7@gmail.com>
2022-10-26 07:19:50 +0600

    added left side nav component and displayed data by fetching data from server

--- src/Pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import Nav from "react-bootstrap/Nav";
+import { Link } from "react-router-dom";
 
 const LeftSideNav = () => {
+  const [courses, setCourses] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/courses")
+      .then((res) => res.json())
+      .then((data) => setCourses(data));
+  }, []);
+
   return (
-    <div>
-      <h1>This is left side nav</h1>
-    </div>
+    <Nav className="flex-column bg-light p-4 rounded">
+      {courses.map((course) => (
+        <Link to={`course/${course.id}`} key={course.id} className="nav-link">
+          {course.name}
+        </Link>
+      ))}
+    </Nav>
   );
 };
 
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -8,7 +8,7 @@ const Main = () => {
   return (
     <div>
       <Header></Header>
-      <div>
+      <div className="py-5">
         <Container>
           <Row>
             <Col lg="3">
