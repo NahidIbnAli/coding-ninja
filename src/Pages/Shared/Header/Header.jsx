9ba012451a866ebe29813d2237a7e5a156fbf432
Nahid Ibn Ali <nahidibnali7@gmail.com>
2022-10-27 02:47:49 +0600

    added route to the logo and courses in navbar

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -2,21 +2,23 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
 import logo from "../../../assets/codingNinja.svg";
 
 const Header = () => {
   return (
-    <Navbar bg="white" expand="lg">
+    <Navbar bg="white" expand="lg" className="sticky-top">
       <Container>
-        <Navbar.Brand href="#home">
+        <Link to="/" className="navbar-brand">
           <img style={{ height: "40px" }} src={logo} alt="" />
           Coding Ninja
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+        </Link>
+        <Navbar.Toggle aria-controls="navbar-nav" />
+        <Navbar.Collapse id="navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Link to="/" className="nav-link">
+              Courses
+            </Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
