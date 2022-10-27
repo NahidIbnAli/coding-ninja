59464fe7e10647382741755f72c3495cc5786ee3
Nahid Ibn Ali <nahidibnali7@gmail.com>
2022-10-27 17:19:33 +0600

    added dark mode toggle button and handler

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
 import logo from "../../../assets/codingNinja.svg";
+import { BsMoon, BsSun } from "react-icons/bs";
 
 const Header = () => {
+  const [toggleDark, setToggleDark] = useState(false);
+  // dark mode toggle handler
+  const handleModeToggle = (event) => {
+    event.preventDefault();
+    setToggleDark(!toggleDark);
+  };
+
   return (
     <Navbar bg="white" expand="lg" className="sticky-top">
       <Container>
@@ -15,7 +23,7 @@ const Header = () => {
         </Link>
         <Navbar.Toggle aria-controls="navbar-nav" />
         <Navbar.Collapse id="navbar-nav">
-          <Nav className="ms-auto">
+          <Nav className="ms-auto gap-3">
             <Link to="/" className="nav-link">
               Courses
             </Link>
@@ -25,6 +33,17 @@ const Header = () => {
             <Link to="/blog" className="nav-link">
               Blog
             </Link>
+            {/* dark mode toggle button */}
+            <button
+              onClick={handleModeToggle}
+              className="nav-link d-flex align-items-center gap-2 btn border-0"
+            >
+              {toggleDark ? (
+                <BsMoon className="fs-5"></BsMoon>
+              ) : (
+                <BsSun className="fs-5"></BsSun>
+              )}
+            </button>
           </Nav>
         </Navbar.Collapse>
       </Container>
